8605855b38eb01e65478f066fdff3d0fdbb6fb74
Lucas Silva <lucas.silva@ati.ma.gov.br>
2024-11-25 14:13:16 -0300

    ajuste

--- src/GenerateBladeFromRequestServiceProvider.php
+++ src/GenerateBladeFromRequestServiceProvider.php
@@ -13,13 +13,6 @@ class GenerateBladeFromRequestServiceProvider extends ServiceProvider
             GenerateBladeRequestCommand::class,
         ]);
 
-//        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'barbosalb96/blade');
-
-//        $this->publishes([
-//            __DIR__ . '/pt_BR.json' => $this->app->langPath() . '/pt_BR.json',
-//            __DIR__ . '/pt_BR' => $this->app->langPath() . '/pt_BR'
-//        ], 'laravel-pt-br-localization');
-//    }
         $this->publishes([
             __DIR__ . '/resources/views' => $this->app->langPath() . 'views/vendor/barbosalb96/blade',
         ], 'views');
@@ -27,6 +20,5 @@ class GenerateBladeFromRequestServiceProvider extends ServiceProvider
 
     public function boot()
     {
-//        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'barbosalb96/blade');
     }
 }
